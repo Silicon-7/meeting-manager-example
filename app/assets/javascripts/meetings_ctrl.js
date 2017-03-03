35d128d09550edf088800f3202a577bb3fa2349a
JackMarx <seriousfools@gmail.com>
2017-03-03 09:40:34 -0600

    final

--- app/assets/javascripts/meetings_ctrl.js
+++ app/assets/javascripts/meetings_ctrl.js
@@ -6,6 +6,52 @@
       $http.get("/api/v1/meetings.json").then(function(response) {
         $scope.meetings = response.data;
       });
+      $http.get("/api/v1/tags.json").then(function(response) {
+        $scope.tags = response.data;
+      });
+    };
+
+    $scope.filterByTag = function(tag) {
+      $scope.tagFilterId = tag ? tag.id : null;
+    };
+
+    $scope.matchTag = function(meeting) {
+      if ($scope.tagFilterId) {
+        for (var i = 0; i < meeting.tags.length; i++) {
+          var tag = meeting.tags[i];
+          if (tag.id === $scope.tagFilterId) {
+            return true;
+          }
+        }
+        return false;
+      } else {
+        return true;
+      }
+    };
+
+    $scope.changeOrderAttribute = function(attribute) {
+      $scope.descending = (attribute !== $scope.orderAttribute) ? false : !$scope.descending;
+      $scope.orderAttribute = attribute;
+    };
+
+    $scope.createMeeting = function(name, address, startTime, endTime, notes, tags) {
+      var params = {
+                    name: name,
+                    address: address,
+                    "start_time": startTime,
+                    "end_time": endTime,
+                    notes: notes,
+                    tags: tags
+                    };
+
+      $http.post("/api/v1/meetings.json", params).then(function(response){
+        $scope.meetings.push(response.data);
+      });
     };
   });
 }());
+
+
+
+
+
--- app/controllers/api/v1/meetings_controller.rb
+++ app/controllers/api/v1/meetings_controller.rb
@@ -2,4 +2,29 @@ class Api::V1::MeetingsController < ApplicationController
   def index
     @meetings = Meeting.all
   end
+
+  def create
+    @meeting = Meeting.new(
+                           name: params[:name],
+                           address: params[:address],
+                           start_time: params[:start_time],
+                           end_time: params[:end_time],
+                           notes: params[:notes]
+                           )
+
+    if @meeting.save
+      tag_ids = params[:tags]
+      if tag_ids
+        tag_ids.each do |tag_id|
+          MeetingTag.create(
+                            meeting_id: @meeting.id,
+                            tag_id: tag_id
+                            )
+        end
+      end
+      render :show
+    else
+      render json: @meeting.errors.full_messages, status: 422
+    end
+  end
 end
